88c8e3fbffafd4cbbbed22aaf427537fb82add01
DrCloudFactory454 <diru@cloudfactory.dk>
2023-11-20 19:18:54 +0100

    new three.js elements modified

--- public/3DModel.jsx
+++ public/3DModel.jsx
@@ -158,7 +158,7 @@ const Moon = () => {
       fragmentShader,
       uniforms: {
         globeTexture: {
-          value: new THREE.TextureLoader().load("/assets/img/earth/movie.webp"),
+          value: new THREE.TextureLoader().load("/assets/img/earth/moon.jpg"),
         },
       },
     })
@@ -211,7 +211,8 @@ const Moon = () => {
   // camera.position.z = 16.7 --> mobile size
 
   if (sphere) {
-    camera.position.z = 17.6;
+    camera.position.z = 60;
+    camera.position.x = -25;
   }
 
   const mouse = {
@@ -374,10 +375,94 @@ const Earth = () => {
 };
 
 
+//Function Avatar -->
 
+const Avatar = () => {
+  // camera function
 
+  const canvasContainer = document.querySelector("#canvasContainerAvatar");
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    70,
+    canvasContainer.offsetWidth / canvasContainer.offsetHeight,
+    0.1,
+    1000
+  );
+  console.log(scene);
 
+  //function to render
 
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    canvas: document.querySelector("#canvasContainerAvatar"), //--> to improve the image
+    alpha: true,
+  });
+
+  //OrbitControls
+  const orbitControls = new OrbitControls(camera, renderer.domElement);
+  orbitControls.enableDamping = true;
+  orbitControls.autoRotate = true;
+  orbitControls.autoRotateSpeed = 6.8;
+  orbitControls.enableZoom = false;
+
+  //Responsive Desing with Three Js
+
+  window.addEventListener("resize", function () {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(width, height);
+  });
+
+  //Responsive Desing with Three Js
+
+  renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight);
+  renderer.setPixelRatio(window.devicePixelRatio); //--> to improve the pixels to the image
+
+  var loader = new GLTFLoader();
+  loader.load("/assets/img/avatar/scene.gltf", function (gltf) {
+    const model = gltf.scene;
+
+    scene.add(model);
+
+    model.position.x = -180;
+  });
+
+  camera.position.z = 290;
+  camera.position.y = 130;
+
+  const mouse = {
+    x: undefined,
+    y: undefined,
+  };
+
+  //animate function
+  const animate = () => {
+    orbitControls.update();
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  };
+
+  // Light
+
+  const ambientalLight = new THREE.AmbientLight(0xffffff, 1);
+  scene.add(ambientalLight);
+
+  const PointLight = new THREE.PointLight(0xffffff, 1);
+  PointLight.position.set(6, 7, 6);
+  scene.add(PointLight);
+
+  animate();
+
+  // function to move the mouse
+
+  addEventListener("mousemove", () => {
+    mouse.x = (event.clientX / innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / innerHeight) * 2 + 1;
+  });
+  // scene.add(sphere);
+};
 
 
 
@@ -408,9 +493,10 @@ const Phoenix = () => {
   //OrbitControls
   const orbitControls = new OrbitControls(camera, renderer.domElement);
   orbitControls.enableDamping = true;
-  //orbitControls.autoRotate = true;
-  orbitControls.autoRotateSpeed = 3.5;
+  orbitControls.autoRotate = true;
+  orbitControls.autoRotateSpeed = 6.5;
   orbitControls.enableZoom = false;
+  
 
   //Responsive Desing with Three Js
 
@@ -432,9 +518,21 @@ const Phoenix = () => {
     const model = gltf.scene;
 
     scene.add(model);
+      model.position.x = -220;
+    //  model.position.y = 100;
+     //model.position.z = 150;
   });
 
-  camera.position.z = 150;
+  // Phoenix Blue Astronaut
+ 
+  camera.position.z = 400;
+  camera.position.y = 40;
+  camera.position.x = 50;
+
+  
+
+  // camera.position.x = 150;
+
 
   const mouse = {
     x: undefined,
@@ -447,12 +545,10 @@ const Phoenix = () => {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     // Rotation
-    //loader.rotation.y += 0.004;
+    // loader.rotation.y += 0.004;
     
   };
 
-  
-
   // Light
 
   const ambientalLight = new THREE.AmbientLight(0xffffff, 1);
@@ -473,98 +569,20 @@ const Phoenix = () => {
   // scene.add(sphere);
 };
 
-//Function Avatar -->
 
-const Avatar = () => {
-  // camera function
 
-  const canvasContainer = document.querySelector("#canvasContainerAvatar");
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    70,
-    canvasContainer.offsetWidth / canvasContainer.offsetHeight,
-    0.1,
-    1000
-  );
-  console.log(scene);
 
-  //function to render
 
-  const renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    canvas: document.querySelector("#canvasContainerAvatar"), //--> to improve the image
-    alpha: true,
-  });
 
-  //OrbitControls
-  const orbitControls = new OrbitControls(camera, renderer.domElement);
-  orbitControls.enableDamping = true;
-  orbitControls.autoRotate = true;
-  orbitControls.autoRotateSpeed = -2.8;
-  orbitControls.enableZoom = false;
 
-  //Responsive Desing with Three Js
-
-  window.addEventListener("resize", function () {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-    renderer.setSize(width, height);
-  });
-
-  //Responsive Desing with Three Js
-
-  renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight);
-  renderer.setPixelRatio(window.devicePixelRatio); //--> to improve the pixels to the image
-
-  var loader = new GLTFLoader();
-  loader.load("/assets/img/avatar/scene.gltf", function (gltf) {
-    const model = gltf.scene;
-
-    scene.add(model);
-  });
-
-  camera.position.z = 130;
-
-  const mouse = {
-    x: undefined,
-    y: undefined,
-  };
-
-  //animate function
-  const animate = () => {
-    orbitControls.update();
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-  };
-
-  // Light
-
-  const ambientalLight = new THREE.AmbientLight(0xffffff, 1);
-  scene.add(ambientalLight);
-
-  const PointLight = new THREE.PointLight(0xffffff, 1);
-  PointLight.position.set(6, 7, 6);
-  scene.add(PointLight);
-
-  animate();
-
-  // function to move the mouse
-
-  addEventListener("mousemove", () => {
-    mouse.x = (event.clientX / innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / innerHeight) * 2 + 1;
-  });
-  // scene.add(sphere);
-};
 
 const Models = () => {
   Earth();
+  Avatar();
   LegoAstronauts();
   Moon();
   Phoenix();
-  Avatar();
+  
 };
 
 Models();
